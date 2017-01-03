9659102f8331a4c398a3eb54acf5392ab2b0ad1d
William Cox <willubyu7@gmail.com>
2017-01-03 16:37:16 -0700

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var messages = [];
+
+var app = express();
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', function(req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date(), userName: "KMalone32", profile_pic: "https://avatars0.githubusercontent.com/u/6438662?v=3&s=40" });
+  res.status(200).json({ messages: messages });
+});
+
+app.listen(3000);
